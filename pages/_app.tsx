c2771c0a640ab03f4f569e9b4e8c7ca0a16d48ad
tomon9086 <tomon9086@gmail.com>
2022-03-05 15:36:38 +0900

    theming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux'
 import { AppProps } from 'next/app'
-import { Global, css } from '@emotion/react'
+import { Global, ThemeProvider, css } from '@emotion/react'
 import 'normalize.css'
 
 import store from '@/store'
@@ -8,28 +8,36 @@ import store from '@/store'
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
-      <Global
-        styles={css`
-          html,
-          body {
-            padding: 0;
-            margin: 0;
-            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-              sans-serif;
+      <ThemeProvider
+        theme={{
+          colors: {
+            primary: '#000'
           }
+        }}
+      >
+        <Component {...pageProps} />
+        <Global
+          styles={css`
+            html,
+            body {
+              padding: 0;
+              margin: 0;
+              font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+            }
 
-          a {
-            color: inherit;
-            text-decoration: none;
-          }
+            a {
+              color: inherit;
+              text-decoration: none;
+            }
 
-          * {
-            box-sizing: border-box;
-          }
-        `}
-      />
+            * {
+              box-sizing: border-box;
+            }
+          `}
+        />
+      </ThemeProvider>
     </Provider>
   )
 }
