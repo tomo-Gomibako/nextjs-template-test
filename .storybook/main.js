082d3eeda3880dd7eeb4f377cf909a168164dabe
tomon9086 <tomon9086@gmail.com>
2022-04-01 22:58:40 +0900

    install typescript preset of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,10 @@ const path = require('path')
 
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
+  core: {
+    builder: 'webpack5'
+  },
+  addons: ['@storybook/preset-typescript'],
   webpackFinal: async (config) => {
     config.resolve.alias = {
       '@': path.resolve(__dirname, '../src')
