73a770579a501109b9d45c6177f47391e8430408
tomon9086 <tomon9086@gmail.com>
2023-05-23 20:50:15 +0900

    uninstall storybook

--- .storybook/preview.js
+++
@@ -1,3 +0,0 @@
-import App from '../src/pages/_app'
-
-export const decorators = [(Story) => <App Component={Story}></App>]
--- stories/Example.stories.tsx
+++
@@ -1,6 +0,0 @@
-export default {
-  title: 'Example'
-}
-
-export const Default = () => <button>default</button>
-export const Disabled = () => <button disabled>disabled</button>
