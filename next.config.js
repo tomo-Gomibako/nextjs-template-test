01f6ca945a6f85d64e4bcb4d8254e2566fb6242b
tomon9086 <tomon9086@gmail.com>
2024-01-01 00:09:48 +0900

    add RuntimeConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  trailingSlash: false,
+  publicRuntimeConfig: {},
+  serverRuntimeConfig: {}
+}
 
 module.exports = nextConfig
